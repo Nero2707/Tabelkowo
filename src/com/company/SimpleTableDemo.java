a6b53f697c677723b05611829392161ec005f451
Nero <michal2707@O2.pl>
2019-05-16 22:41:05 +0200

    Przeniesienie metody

--- src/com/company/SimpleTableDemo.java
+++ src/com/company/SimpleTableDemo.java
@@ -1,47 +1,10 @@
-/*
- * Copyright (c) 1995, 2008, Oracle and/or its affiliates. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *   - Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *   - Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *
- *   - Neither the name of Oracle or the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
- * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package com.company;
 
-/*
- * SimpleTableDemo.java requires no other files.
- */
-
 import javax.swing.*;
-import java.awt.Dimension;
-import java.awt.GridLayout;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -49,7 +12,7 @@ import java.util.stream.Stream;
 
 public class SimpleTableDemo extends JPanel {
     private boolean DEBUG = false;
-
+    private JTable table;
     public SimpleTableDemo() {
         super(new GridLayout(1,0));
 
@@ -69,20 +32,12 @@ public class SimpleTableDemo extends JPanel {
                 "System operacyjny",
                 "Napęd optyczny"};
 
-        Object[][] data = { };
 
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        final JTable table = new JTable(data, columnNames);
+        table = new JTable(new DefaultTableModel(columnNames,0));
         table.setPreferredScrollableViewportSize(new Dimension(800, 600));
         table.setFillsViewportHeight(true);
 
-        if (DEBUG) {
-            table.addMouseListener(new MouseAdapter() {
-                public void mouseClicked(MouseEvent e) {
-                    printDebugData(table);
-                }
-            });
-        }
         JButton wczytajDaneTxtButton = new JButton("Wczytaj dane z TXT");
         JButton wczytajDaneXmlButton = new JButton("Wczytaj dane z XML");
         JButton zapiszDaneTxtButton = new JButton("Zapisz dane do TXT");
@@ -93,12 +48,7 @@ public class SimpleTableDemo extends JPanel {
         buttonsPanel.add(zapiszDaneTxtButton);
         buttonsPanel.add(zapiszDaneXmlButton);
         add(buttonsPanel);
-        //Create the scroll pane and add the table to it.
-
         JScrollPane scrollPane = new JScrollPane(table);
-
-
-        //Add the scroll pane to this panel.
         add(scrollPane);
         wczytajDaneTxtButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -112,52 +62,32 @@ public class SimpleTableDemo extends JPanel {
     }
 
     private void wczytajTXT() {
+        DefaultTableModel model = (DefaultTableModel) table.getModel();
         try (Stream<String> stream = Files.lines(Paths.get("laptopy.txt"))) {
             stream.forEach(System.out::println);
+            model.addRow(new Object[]{"abc","abc","abc"});
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
-    private void printDebugData(JTable table) {
-        int numRows = table.getRowCount();
-        int numCols = table.getColumnCount();
-        javax.swing.table.TableModel model = table.getModel();
 
-        System.out.println("Value of data: ");
-        for (int i=0; i < numRows; i++) {
-            System.out.print("    row " + i + ":");
-            for (int j=0; j < numCols; j++) {
-                System.out.print("  " + model.getValueAt(i, j));
-            }
-            System.out.println();
-        }
-        System.out.println("--------------------------");
-    }
 
-    /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from the
-     * event-dispatching thread.
-     */
+
     private static void createAndShowGUI() {
-        //Create and set up the window.
-        JFrame frame = new JFrame("SimpleTableDemo");
+
+        JFrame frame = new JFrame("Tabelkowo");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //Create and set up the content pane.
         SimpleTableDemo newContentPane = new SimpleTableDemo();
-        newContentPane.setOpaque(true); //content panes must be opaque
+        newContentPane.setOpaque(true);
         frame.setContentPane(newContentPane);
 
-        //Display the window.
+
         frame.pack();
         frame.setVisible(true);
     }
 
     public static void main(String[] args) {
-        //Schedule a job for the event-dispatching thread:
-        //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 createAndShowGUI();
